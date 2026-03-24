f3054911b57979c5a1ea1db5386d2b73cdae77af
Jacob Andrean <jacob.andrean@icloud.com>
2026-03-24 12:32:47 +0700

    Initial Commit

---
+++ AcademyAlarm/AcademyAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademyAlarmApp.swift
+//  AcademyAlarm
+//
+//  Created by User on 24/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AcademyAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
